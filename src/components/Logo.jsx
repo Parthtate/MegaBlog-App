26277454a7eb4735c2c0012a49f4328a90ad42e8
Parthtate <parthtate575@gmail.com>
2025-02-02 20:33:46 +0530

    fix logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import logoImg from '../assets/logoImg.webp'
 function Logo({width = "100px"}) {
   return (
-    <div className='text-white '>
-      <img src="src\components\logo\WhatsApp Image 2025-02-01 at 18.01.41_d714bec0.jpg" alt="logo" width={width} />
+    <div>
+       <img 
+         src={logoImg} 
+         alt="Logo" 
+         style={{ 
+           width, 
+           borderRadius: '10%' 
+         }} 
+       />
     </div>
   )
 }
